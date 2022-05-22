b6475a4dd211256f50d7fecd82e7e5d5dd9b8c7a
akazwz <akazwz@pm.me>
2022-05-22 11:39:48 +0800

    styled search box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 import {
 	Box,
-	Center, HStack,
+	Center,
+	HStack,
 	IconButton,
 	Image,
 	Input,
-	useColorMode
+	useColorMode, useColorModeValue
 } from '@chakra-ui/react'
 import Head from 'next/head'
 import { MoonIcon, SearchIcon, SunIcon } from '@chakra-ui/icons'
@@ -48,18 +49,24 @@ const Home: NextPage = () => {
 		return () => clearInterval(interval)
 	}, [])
 
-	const baseSize = 36
+	const baseSize = 30
+
+	const inputActiveBg = useColorModeValue('gray.50', 'blackAlpha.500')
+	
+	const handleSearchButtonClock = () => {
+	  
+	}
 
 	return (
 		<>
 			<Head>
 				<title>Start Page</title>
 			</Head>
-			<Box>
-				<Center m={7}>
+			<Box padding={3}>
+				<Center>
 					<IconButton onClick={toggleColorMode} aria-label={'toggle color mode'} icon={toggleColorModeIcon} />
 				</Center>
-				<Center m={7}>
+				<Center m={3}>
 					<NumbersClock value={hours} isDark={colorMode === 'dark'} size={baseSize} />
 					<SymbolClock value={':'} isDark={colorMode === 'dark'} size={baseSize} />
 					<NumbersClock value={minutes} isDark={colorMode === 'dark'} size={baseSize} />
@@ -67,11 +74,38 @@ const Home: NextPage = () => {
 					<NumbersClock value={seconds} isDark={colorMode === 'dark'} size={baseSize} />
 				</Center>
 				<Center minHeight="30vh">
-					<HStack spacing={0} borderWidth={1} rounded="lg" padding={1}>
-						<IconButton size="lg" icon={<Image src={'/google.png'} draggable={false} alt="" />}
-						            aria-label={'search regine'} variant="ghost" />
-						<Input size="lg" rounded="lg" borderStyle="hidden" />
-						<IconButton size="lg" aria-label={'search'} icon={<SearchIcon />} variant="ghost" />
+					<HStack
+						spacing={0}
+						borderWidth={1}
+						rounded="lg"
+						padding={1}
+						backgroundColor={'transparent'}
+						_focusWithin={{
+							backgroundColor: inputActiveBg
+						}}
+						_hover={{
+							backgroundColor: inputActiveBg
+						}}
+						width={'lg'}
+					>
+						<IconButton
+							icon={<Image src={'/google.png'} draggable={false} alt="" />}
+							aria-label={'search regine'} variant="ghost"
+						/>
+						<Input
+							outline={0}
+							backgroundColor={'transparent'}
+							_focus={{
+								outline: 'none'
+							}}
+							rounded="lg"
+							border={'none'}
+						/>
+						<IconButton
+							aria-label={'search'}
+							icon={<SearchIcon fontSize="xl"/>}
+							variant="ghost"
+						/>
 					</HStack>
 				</Center>
 			</Box>
